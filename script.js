8cacb1aaa9e54184fc112c234564ee1390f8c248
takfarinas15 <takfarinas.aberkane@gmail.com>
2026-05-10 14:40:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const input = document.querySelector("#Ipt");
+const button = document.getElementById("btn");
+const todotext = document.querySelector("#todo");
+
+let tache = "";
+input.addEventListener("input", (e) => {
+  tache = e.target.value.trim();
+});
+
+button.addEventListener("click", () => {
+  if (tache === "") {
+    return;
+  } else {
+    todotext.innerHTML += `<li>${tache} <button class="deleteBtn">Supprimer</button> </li>`;
+    input.value = "";
+    tache = "";
+  }
+});
+
+todotext.addEventListener("click", (e) => {
+  if (e.target.classList.contains("deleteBtn")) e.target.closest("li").remove();
+  else {
+    console.log("ouii");
+  }
+});
